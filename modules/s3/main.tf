70954fa139dc29a12d95f25be835d02f63a1b758
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-23 22:36:42 +0530

    changed s3-policy

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -42,7 +42,7 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
-  bucket = aws_s3_bucket.cloudtrail_bucket.id
+  bucket = aws_s3_bucket.cloudtrail_bucket.bucket  # Use `.bucket` instead of `.id`
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -54,7 +54,7 @@ resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
           Service = "cloudtrail.amazonaws.com"
         },
         Action    = "s3:GetBucketAcl",
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.id}"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.bucket}"
       },
       {
         Sid       = "AWSCloudTrailWrite",
@@ -63,7 +63,7 @@ resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
           Service = "cloudtrail.amazonaws.com"
         },
         Action    = "s3:PutObject",
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.id}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.bucket}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
         Condition = {
           StringEquals = {
             "s3:x-amz-acl" = "bucket-owner-full-control"
