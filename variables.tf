6cfde044fc34dc7c20e9114bd27ecf0783e6bf67
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-23 21:39:34 +0530

    modified default values

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "cloudtrail_bucket_name" {
   description = "name of the s3 bucket"
   type = string
-  default = "demo-cloudtrail"
+  default = "demo-143-latest"
 }
 
 variable "cloudtrail_log_group_name" {
   description = "cloudtrail log group name"
   type = string
-  default = "demo-cloudtrail-log-group"
+  default = "demo-143-latest"
 }
 
 variable "sns_topic_name" {
   description = "SNS topic name"
   type = string
-  default = "cloud-custom-metric-topic"
+  default = "custom-metric-topic"
 }
 
 variable "cloudtrail_name" {
   description = "Name of the cloudtrail"
   type = string
-  default = "demo-multi-region-trail"
+  default = "multi-region-cloudtrail"
 }
