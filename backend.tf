0b15cd57ca8039f87076047209c13809e47f2f9a
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-23 21:24:24 +0530

    changed key in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "tf-bucket-prod1"
-    key          = "uc11-nginx/terraform.tfstate"
+    key          = "uc14-cloudtrail/terraform.tfstate"
     region       = "us-east-1"
     #use_lockfile = true
     encrypt      = true
