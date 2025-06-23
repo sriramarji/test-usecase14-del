8280cee31e5cefb1b9436afcbb93be74e65faae8
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-23 23:48:08 +0530

    test

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -41,7 +41,7 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
 
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
+/*resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
   bucket = "demo-cloudtrail-latest-1997"
 
   policy = jsonencode({
@@ -67,6 +67,40 @@ resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
         Condition: {
           StringEquals: {
             "s3:x-amz-acl": "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}*/
+
+
+resource "aws_s3_bucket_policy" "trail_bucket_policy" {
+  bucket = aws_s3_bucket.cloudtrail_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailAclCheck",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action    = "s3:GetBucketAcl",
+        Resource  = aws_s3_bucket.cloudtrail_bucket.arn
+      },
+      {
+        Sid       = "AWSCloudTrailWrite",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action    = "s3:PutObject",
+        Resource  = "${aws_s3_bucket.cloudtrail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
           }
         }
       }
