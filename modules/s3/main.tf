f6f67a653df49406f98a780e758df54cae77707b
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-23 23:54:38 +0530

    Final Testing

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -2,12 +2,12 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
   bucket = var.cloudtrail_bucket_name
 
   tags = {
-    Name = var.cloudtrail_bucket_name
+    Name = "var.cloudtrail_bucket_name"
   }
 }
 
-/*resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
-  bucket = "bayer-demo-cloudtrail-logs"
+resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
+  bucket = "demo-cloudtrail-latest-1997"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -19,7 +19,7 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
           Service = "cloudtrail.amazonaws.com"
         }
         Action   = "s3:GetBucketAcl"
-        Resource = "arn:aws:s3:::bayer-demo-cloudtrail-logs"
+        Resource = "arn:aws:s3:::demo-cloudtrail-latest-1997"
       },
       {
         Sid    = "AWSCloudTrailWrite"
@@ -28,76 +28,7 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
           Service = "cloudtrail.amazonaws.com"
         }
         Action   = "s3:PutObject"
-        Resource = "arn:aws:s3:::bayer-demo-cloudtrail-logs/AWSLogs/058264249757/*"
-        Condition = {
-          StringEquals = {
-            "s3:x-amz-acl" = "bucket-owner-full-control"
-          }
-        }
-      }
-    ]
-  })
-}*/
-
-data "aws_caller_identity" "current" {}
-
-/*resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
-  bucket = "demo-cloudtrail-latest-1997"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid: "AWSCloudTrailAclCheck",
-        Effect: "Allow",
-        Principal: {
-          Service: "cloudtrail.amazonaws.com"
-        },
-        Action: "s3:GetBucketAcl",
-        Resource: "arn:aws:s3:::demo-cloudtrail-latest-1997"
-      },
-      {
-        Sid: "AWSCloudTrailWrite",
-        Effect: "Allow",
-        Principal: {
-          Service: "cloudtrail.amazonaws.com"
-        },
-        Action: "s3:PutObject",
-        Resource: "arn:aws:s3:::demo-cloudtrail-latest-1997/AWSLogs/211125784755/*",
-        Condition: {
-          StringEquals: {
-            "s3:x-amz-acl": "bucket-owner-full-control"
-          }
-        }
-      }
-    ]
-  })
-}*/
-
-
-resource "aws_s3_bucket_policy" "trail_bucket_policy" {
-  bucket = aws_s3_bucket.cloudtrail_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "AWSCloudTrailAclCheck",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudtrail.amazonaws.com"
-        },
-        Action    = "s3:GetBucketAcl",
-        Resource  = aws_s3_bucket.cloudtrail_bucket.arn
-      },
-      {
-        Sid       = "AWSCloudTrailWrite",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudtrail.amazonaws.com"
-        },
-        Action    = "s3:PutObject",
-        Resource  = "${aws_s3_bucket.cloudtrail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Resource = "arn:aws:s3:::demo-cloudtrail-latest-1997/AWSLogs/211125784755/*"
         Condition = {
           StringEquals = {
             "s3:x-amz-acl" = "bucket-owner-full-control"
@@ -107,3 +38,6 @@ resource "aws_s3_bucket_policy" "trail_bucket_policy" {
     ]
   })
 }
+
+
+data "aws_caller_identity" "current" {}
