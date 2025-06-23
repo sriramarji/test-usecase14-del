d9290436b4f6f0f15ba6d9b2ab88032fc3f7d223
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-23 21:49:39 +0530

    modified values and modules3-main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -61,7 +61,7 @@ resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
           Service = "cloudtrail.amazonaws.com"
         },
         Action   = "s3:PutObject",
-        Resource = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_bucket.id}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Resource = "arn:aws:s3:::demo-cloudtrail-latest-1997/AWSLogs/211125784755/*",
         Condition = {
           StringEquals = {
             "s3:x-amz-acl" = "bucket-owner-full-control"
--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "cloudtrail_bucket_name" {
   description = "name of the s3 bucket"
   type = string
-  default = "demo-143-latest"
+  default = "demo-cloudtrail-latest-1997"
 }
 
 variable "cloudtrail_log_group_name" {
   description = "cloudtrail log group name"
   type = string
-  default = "demo-143-latest"
+  default = "demo-cloudtrail-logs-1997"
 }
 
 variable "sns_topic_name" {
