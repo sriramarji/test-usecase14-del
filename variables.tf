2108b3bad60a2eb2e702f87f8d68553725a28f63
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-23 20:00:55 +0530

    modified syntax

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "cloudtrail_bucket_name" {
   description = "name of the s3 bucket"
   type = string
-  default = "bayer-demo-cloudtrail-logs"
+  default = "demo-cloudtrail-logs"
 }
 
 variable "cloudtrail_log_group_name" {
   description = "cloudtrail log group name"
   type = string
-  default = "bayer-demo-cloudtrail-log-group"
+  default = "demo-cloudtrail-log-group"
 }
 
 variable "sns_topic_name" {
   description = "SNS topic name"
   type = string
-  default = "cloudwatch-custom-metric-topic"
+  default = "cloud-custom-metric-topic"
 }
 
 variable "cloudtrail_name" {
   description = "Name of the cloudtrail"
   type = string
-  default = "jp-demo-multi-region-trail"
+  default = "demo-multi-region-trail"
 }
