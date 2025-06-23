da5beb7f0d5c4f760351630beca34337643ae9be
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-23 23:03:00 +0530

    corrected s3-main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
   bucket = var.cloudtrail_bucket_name
 
   tags = {
-    Name = "var.cloudtrail_bucket_name"
+    Name = var.cloudtrail_bucket_name
   }
 }
 
@@ -42,7 +42,7 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
-  bucket = "demo-cloudtrail-latest-1997"
+  bucket = "bayer-demo-cloudtrail-logs"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -54,7 +54,7 @@ resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
           Service = "cloudtrail.amazonaws.com"
         }
         Action   = "s3:GetBucketAcl"
-        Resource = "arn:aws:s3:::demo-cloudtrail-latest-1997
+        Resource = "arn:aws:s3:::bayer-demo-cloudtrail-logs"
       },
       {
         Sid    = "AWSCloudTrailWrite"
@@ -63,7 +63,7 @@ resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
           Service = "cloudtrail.amazonaws.com"
         }
         Action   = "s3:PutObject"
-        Resource = "arn:aws:s3:::demo-cloudtrail-latest-1997/AWSLogs/211125784755/*"
+        Resource = "arn:aws:s3:::bayer-demo-cloudtrail-logs/AWSLogs/058264249757/*"
         Condition = {
           StringEquals = {
             "s3:x-amz-acl" = "bucket-owner-full-control"
